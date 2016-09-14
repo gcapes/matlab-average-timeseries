756f1936f3c078cbdbf41b91db2f2202b43486be
Gerard Capes <gerardcapes@gmail.com>
2016-09-14 15:46:27 +0100

    Make separate tstart and tend arrays instead of tcuts

--- tcutsfrommids.m
+++ tcutsfrommids.m
@@ -1,10 +1,8 @@
-% Limitations are that if there are any missing data points (time stamps)
-% then the averaging interval will be incorrect
-function [tstart,tend]= tcutsfrommids(tmid,interval)
+function [tstart,tend]= tcutsfrommids(tmid,time_step)
 % tmid is the time mid-points
 % interval is the required averaging interval in the same units as tmid
 % The averaging will happen over tmid-0.5*interval to tmid+0.5*interval
 % i.e. cuts are half way between the mid-points.
 
-tstart=tmid-0.5*interval;
-tend=tmid+0.5*interval;
+tstart=tmid-0.5*time_step;
+tend=tmid+0.5*time_step;
